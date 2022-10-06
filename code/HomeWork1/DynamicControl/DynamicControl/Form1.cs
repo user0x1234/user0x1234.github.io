8fa0db87b4fae8b2e8725930afbac789799bddbe
user0x1234 <user0x1234@gmail.com>
2022-10-07 00:42:04 +0200

    HomeWork1

--- code/HomeWork1/DynamicControl/DynamicControl/Form1.cs
+++ code/HomeWork1/DynamicControl/DynamicControl/Form1.cs
@@ -6,5 +6,35 @@ namespace DynamicControl
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int startPosition = 100;
+            int endPosition = 10;
+            for (int i = 0; i<=7; i++)
+            {
+                Label l = addLabel(i, startPosition, endPosition);
+                this.Controls.Add(l);
+                endPosition += 70;
+            }
+        }
+
+        Label addLabel(int i, int start, int end) 
+        {
+            Label l = new Label();
+            l.Name = "label" + i.ToString();
+            l.Text = "label" + i.ToString();
+            l.ForeColor = Color.White;
+            l.BackColor = Color.Green;
+            l.Font = new Font("Serif", 24, FontStyle.Bold);
+            l.Width = 170;
+            l.Height = 80;
+            l.Location = new Point(start, end);
+            l.TextAlign = ContentAlignment.MiddleCenter;
+            l.Margin = new Padding(5);
+
+            return l;
+
+        }
     }
 }
