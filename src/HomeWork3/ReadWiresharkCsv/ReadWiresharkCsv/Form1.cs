eb965e0be1667f94e40f474de739aa0b80ea4c7e
user0x1234 <user0x1234@gmail.com>
2022-10-21 10:28:11 +0200

    HomeWork3

---
+++ src/HomeWork3/ReadWiresharkCsv/ReadWiresharkCsv/Form1.cs
@@ -0,0 +1,25 @@
+using System.Linq.Expressions;
+
+namespace ReadWiresharkCsv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog() { Filter = "*.cvs", ValidateNames = true })
+            { 
+                if (ofd.)
+            }
+        }
+    }
+}
